3f1cf7c8b06ffcabbe2f5287f2fa1dac25e6fe9b
AryanSingh2k4 <aryanvsingh641@gmail.com>
2026-05-28 22:44:00 +0530

    robot.ts change

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,5 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: ["/api/", "/_next/", "/static/"],
     },
     sitemap: "https://pixelfinds.vercel.app/sitemap.xml",
+    host: "https://pixelfinds.vercel.app",
   };
 }
